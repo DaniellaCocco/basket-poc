ccd45b8d93b757aabf02928bdff9bc3b8327732b
Daniella Cocco <d@daniellacoc.co>
2018-03-05 20:55:43 +0100

    Remove console.logs

--- public/script.js
+++ public/script.js
@@ -17,8 +17,6 @@ changeProductAmount = (id, type) => {
     fetch(`http://localhost:8081/products/${id}`)
         .then(res => res.json())
         .then(data => {
-            console.log('Received amount:', data.amount);
-
             // Calculate the new to-be amount
             newAmount = calculateNewAmount(data.amount, type);
             fetch(`http://localhost:8081/products/${id}`, {
@@ -28,7 +26,6 @@ changeProductAmount = (id, type) => {
             })
                 .then(res => res.json())
                 .then(data => {
-                    console.log('Received updated amount:', data.amount);
                     updateBasket();
                 })
                 .catch(err => console.error(err));
@@ -52,8 +49,6 @@ calculateProductPrice = (amount, price, discount) => {
     // If there's no discount defined at all we can return early and not execute this code any further
     if (!discount) return amount * price;
 
-    console.log('calculateProductPrice params:', amount, price, discount);
-    console.log('discount:', discount[0], 'for', discount[1]);
     // Check the passed amount against multiples of the defined discount amount value
     if (amount % discount[0] === 0) { // = Yes we should apply a discount
         const amountToCalculateWith = amount * (discount[1] / discount[0]);
@@ -70,10 +65,8 @@ updateTotalDue = (products) => {
     let total = 0;
     products.map(product => {
         const { name, amount, price, discount } = product;
-        console.log(name, amount, '*', price, '=', amount * price);
         total = total + calculateProductPrice(amount, price, discount);
     });
-    console.log('Total:', total);
     document.getElementById('total').innerHTML = formatCurrency(total);
 };
 
@@ -91,8 +84,6 @@ updateBasket = () => {
     fetch('http://localhost:8081/products')
         .then(res => res.json())
         .then(data => {
-            console.log('Received:', data);
-
             // Clear basket
             document.getElementById('basket').innerHTML = '';
 
