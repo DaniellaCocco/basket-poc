38b620ed5ec5ce613b90e450c8849229af4f7261
Daniella Cocco <d@daniellacoc.co>
2018-03-05 19:42:33 +0100

    Update basket

--- public/script.js
+++ public/script.js
@@ -9,5 +9,27 @@
 // Calculate total due
 
 // Update basket
-// 1 - Get new basket
-// 2 - Display basket
+updateBasket = () => {
+    // Get new basket
+    fetch('http://localhost:8081/products')
+        .then(res => res.json())
+        .then(data => {
+            console.log('Received:', data);
+
+            // Build product list
+            return data.map(product => {
+                const { name, price, amount } = product;
+                const productItem = document.createElement('p');
+                productItem.innerText = `${name} ${price} ${amount}`;
+
+                // Mount product to DOM
+                if (amount > 0) {
+                    document.getElementById('app').appendChild(productItem);
+                }
+            })
+        })
+        .catch(err => console.error(err));
+};
+
+// Init
+updateBasket();
