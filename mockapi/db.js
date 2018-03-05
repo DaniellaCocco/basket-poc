a38e0f1fb6c9343270491426975fb39a80123ff7
Daniella Cocco <d@daniellacoc.co>
2018-03-05 21:05:45 +0100

    Mock API notes

--- mockapi/db.js
+++ mockapi/db.js
@@ -1,11 +1,22 @@
 // Little helper function which takes care of the generation of a new basket every time the mock API is launched
 const randomAmount = () => Math.floor(Math.random() * 10);
 
+// NOTE: This isn't a "proper" schema since most of the product details are fixed and don't need to be generated.
+// If we were to generate basket contents entirely, including product names, prices, etc then a fully fledged JSON schema
+// would have been helpful. Other helpful libs would have been:
+// - JSON Schema Faker: https://github.com/json-schema-faker/json-schema-faker
+// - Faker.js: https://github.com/marak/Faker.js
+// - Chance.js: https://github.com/chancejs/chancejs
+// However for the basic purpose of generating a random "basket" simply randomising the 'amount' key will do for now.
 const data = {
+  // NOTE: In a real-world app you'd have separate endpoints for the basket and the products. The basket would then only
+  // contain the id's of the products and refer to the products that way.
   "products": [
     {
       "id": 1,
       "name": "Apples",
+      // NOTE: Assuming a real DB would store monetary values in cents so as to use integers.
+      // We have a formatCurrency() helper function defined in script.js for prettier display to the end user.
       "price": 25,
       "discount": null,
       "amount": randomAmount()
@@ -21,6 +32,10 @@ const data = {
       "id": 3,
       "name": "Kiwis",
       "price": 15,
+      // NOTE: The 'discount' array of [5,3] means "5 for the price of 3". This is a little cryptic; in a real-world app
+      // you'd use two separate, appropriately named keys to improve code maintainability as well as readability. It's
+      // also not as stable as you'd want it to be because the exact order the array would change its meaning significantly,
+      // and is something that could easily change without being noticed, causing hard-to-trace bugs.
       "discount": [
         5,
         3
